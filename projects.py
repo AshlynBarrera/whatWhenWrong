4de0e32ad60d18d61652daaf8bbc8b6b1884a583
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-16 12:25:05 -0600

    Debugging what when wrong in this program,inital debugging complete.

---
+++ projects.py
@@ -0,0 +1,25 @@
+
+
+
+score = 10
+
+# If the score is zero or less
+
+#1 had misspelled  variable 
+if score <= 0: 
+  # Output to the user that they lose
+
+   # 2 was missing ' " '
+  print("You Lose!") 
+
+#3 was missing a collin 
+else: 
+  # Output - You Win! - and the score to the user
+  print("You Win!")
+  print("Your score is: " + score ) # 4  print spelled wrong. 
+  print(score)
+
+  # Wish the user good luck next time (If they win AND if they lose)
+print("Good luck next time.") 
+
+# 5  this print should be out of the else statement 
